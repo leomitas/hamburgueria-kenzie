099cccfd36ffcd6b83de3b8413598a0f27ddb528
leomitas <leonardo.abreu.dev@gmail.com>
2023-01-03 12:10:26 -0300

    hotfix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom";
-import HomePage from './pages/home';
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 
 const RoutesComponents = () => {
   return (
     <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='*' element={<Navigate to={'/'} /> } />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default RoutesComponents
+export default RoutesComponents;
